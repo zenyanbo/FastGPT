b67080b9c3484b468ea04985e8776e2e2b7830c4
曾彦博 <34698908+zenyanbo@users.noreply.github.com>
2024-03-29 18:49:00 +0800

    Update queryExtension.ts

--- packages/service/core/ai/functions/queryExtension.ts
+++ packages/service/core/ai/functions/queryExtension.ts
@@ -19,13 +19,12 @@ Finally, refine and select the most relevant, professional and clear retrieval t
 Notice: "retrieval terms" Always are in English.
 
 ## Example:
-### If the question is unclear and broad, generate more and specified "retrieval terms" from different aspects.
 ----------------
 History: 
 """
 """
 Original question: What are the paths followed by free-falling particles in rotating spacetime?
-Retrieval terms: ["Introduce Kerr geodesic", "From Constants of motion to seperable Kerr geodesic equation and solutions","Orbital dynamics: properties of Kerr geodesic and Orbit parametrization","Other formalism: hamiltonian description of geodesic motion"]
+Retrieval terms: ["Introduce Kerr geodesic", "From Constants of motion to seperable Kerr geodesic equation and solutions","Describe orbital dynamics and properties of Kerr geodesic","what is hamiltonian description of geodesic motion?"]
 ----------------
 History: 
 """
@@ -33,7 +32,7 @@ Q: Conversation history.
 A: The current conversation is about the introduction and mathematical derivation of gauge field theory, etc.
 """
 Original question: I don't understand.
-Retrieval terms: ["Give review of gauge symmetry and gauge field theory, including background, development, application and challenges","equations or example of gague field theory: Yang–Mills Lagrangian for the gauge field, Scalar O(n) gauge theory"]
+Retrieval terms: ["Give review of gauge symmetry and gauge field theory from the perspectives of motivation, development, application and challenges","What is the key concept/euqation/feature of gauge field theory?","What is the core principles/framwork/ of gauge field theory?","Give simple example of gague field theory, Scalar O(n) gauge theory"]
 ----------------
 History: 
 """
@@ -43,7 +42,7 @@ Q: What do you think is the most likely breakthrough point in quantum gravity at
 A: I think key point is black hole information paradox, which poses a significant challenge because it suggests a breakdown in our understanding of fundamental physics, particularly...
 """
 Original question: Tell me about it
-Retrieval terms: ["Give review, development and solutions of black hole information paradox","Hawking radiation leads to black hole evaporation, and the Page curve tracks the process of information loss","Susskind and Maldacena's ER=EPR conjecture suggests wormholes may resolve the black hole information paradox by linking them to entangled particles"]
+Retrieval terms: ["Give review of black hole information paradox involve development, solutions and arguement","What is the relationship between Hawking radiation and information loss?","How does Page curve solve the problem of information loss?","black hole information paradox serves as a valuable tool for guiding the establishment of quantum gravity"]
 ----------------
 History: 
 """
@@ -51,24 +50,16 @@ Q: What is black hole thermodynamics?
 A: The four laws of black hole mechanics are physical properties that black holes are believed to satisfy. The laws, analogous to the laws of thermodynamics, were discovered by Jacob Bekenstein, Brandon Carter, and James Bardeen. Further considerations were made by Stephen Hawking......
 """
 Original question: In Kerr-Newman black hole case, derive the first law of black hole thermodynamics from smarr-like relation.
-Retrieval terms: ["Derive first law of thermodynamics from smarr-like relation of Kerr-Newman black hole."]
+Retrieval terms: ["Derive first law of thermodynamics from smarr-like relation of Kerr-Newman black hole.","What is the relationships between first law of thermodynamics and smarr-like relation?"]
 
-### If the question is specific and clear, generate fewer and more relevant "retrieval terms". Even you can generate nothing.
+### If the question is specific and clear, generate more deeper and specified "retrieval terms".
 ----------------
 History: 
 """
 """
 Original question: using quantum field theory to derive Hawking radiation.
-Retrieval terms: ["Black hole thermodynamics: derive Hawking radiation by quantum field theory in curved spacetime"]
+Retrieval terms: ["Black hole thermodynamics: derive Hawking radiation by quantum field theory in curved spacetime","Hawking radiation derivation involves mode expandsion of field in curved spacetime, applying the Bogoliubov transformation, and deriving the thermal radiation spectrum."]
 ----------------
-History: 
-"""
-Q: Conversation history.
-A: The current conversation is about the conformal transformation in gravity.
-"""
-Original question: Prove that under conformal transformation, the Ricci curvature scalar of d-dim spacetime satisfies the following formulas.
-Retrieval terms: ["How does the Ricci curvature scalar change under d-dim spacetime conformal transformation?"]
-
 # Initialization
 Please refer to the above example and requirements, let's begin.
 
