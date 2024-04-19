ab88549f6b76fb2899a3ddb8098faf3bec7a9706
曾彦博 <34698908+zenyanbo@users.noreply.github.com>
2024-04-19 20:41:34 +0800

    Update queryExtension.ts

--- packages/service/core/ai/functions/queryExtension.ts
+++ packages/service/core/ai/functions/queryExtension.ts
@@ -10,36 +10,47 @@ import { chatValue2RuntimePrompt } from '@fastgpt/global/core/chat/adapt';
     可以根据上下文，消除指代性问题以及扩展问题，利于检索。
 */
 
-const defaultPrompt = `As an information retrieval assistant specializing in the field of theoretical physics and mathematics, your task is to analyze an "original question" along with relevant historical data in order to generate a list of optimized "retrieval terms" that will improve problem expression, the semantic richness and accuracy of information retrieval for the given query. 
+const defaultPrompt = `As an information retrieval assistant specializing in the field of theoretical physics and mathematics, your need to analyze an "Task" along with relevant historical data in order to generate a list of optimized "retrieval terms" that will improve [Task]'s expression, the semantic richness and accuracy of information retrieval for the given query. 
 
 ## Process
-First, carefully analyze the "original question" and historical data, identify goal and related concepts from "original question". 
-Next, determine whether "original question" needs to be optimized or generalized. If the problem is specific or cutting-edge, don't generate optimized "retrieval terms".
-Then, if you think "original question" needs to be optimized. Focusing on the key concepts, entities, and relationships mentioned. Brainstorming to generate potential "retrieval terms" from different perspectives that capture the core meaning of the "original question".
-Finally, according to the following examples and requirements, refine and select the most professional, clear and specific "retrieval terms" from your brainstorming. 
+First, carefully analyze the "Task" and historical data, identify goal and related concepts from "Task". 
+Next, classify [Task] and respond accordingly. Because some types of [Task] require no or little "retrieval terms".
+Then, if you needs "retrieval terms". Focusing on the key concepts, entities, and relationships mentioned. Brainstorming to generate potential "retrieval terms" from different perspectives that capture the core meaning of the "Task".
+Finally, refine and select the most professional, clear and specific "retrieval terms" from your brainstorming. 
+
+### Category of [Task] and the number of "retrieval terms"
+1. Explanation/Understand broad concept/topic: generate 3~4 "retrieval terms"
+2. Ambiguous and unprofessional expression: generate 3~4 "retrieval terms"
+3. Understand professional and specific question: generate 1~2 "retrieval terms"
+4. Involve specific details: generate 0~1 "retrieval terms"
+5. Unfamiliar knowledge: no "retrieval terms"
+6. Others: no "retrieval terms"
 
 ## Example:
 ----------------
 History: 
 """
 """
-Original question: Introduce Kerr geodesic.
-Retrieval terms: ["In the form of lecture, please introduce Kerr geodesic from Kerr spacetime and its symmetry, 'geodesic constants of motion' and 'seperable Kerr geodesic equation', as well as its analytical solutions.","Give a comprehensive description of orbital dynamics and properties of 'bound Kerr geodesic', such as 'Mino/BL frequecy'.","We can find, for periodic and quasi-periodic systems, 'action-angle formailsm' of bound Kerr geodesic is very suitable."]
+Task: What is a free-falling orbit near a spinning black hole?
+Category: 1,2
+Retrieval terms: ["In the form of lecture, introduce Kerr geodesic from Kerr spacetime and its symmetry, 'geodesic constants of motion' and 'seperable Kerr geodesic equation', as well as its analytical solutions.","Give a comprehensive description of orbital dynamics and properties of 'bound Kerr geodesic', such as 'Mino/BL frequecy'.","For periodic and quasi-periodic systems, give 'action-angle formailsm' of bound Kerr geodesic."]
 ----------------
 History: 
 """
 Q: Introduce Kerr spacetime in detail.
 A: Ok, Let's discuss the Kerr spacetime and its symetry, ......
 """
-Original question: So, further introduce Kerr geodesic.
-Retrieval terms: [In the form of lecture, please introduce Kerr geodesic from 'constants of motion' associated with Killing vector/tensor, and seperable Kerr geodesic equation, as well as its analytical solutions.","Give a comprehensive description of orbital dynamics and properties of 'bound Kerr geodesic', such as 'Mino/BL frequecy'.","We can find, for periodic and quasi-periodic systems, 'action-angle formailsm' of bound Kerr geodesic."]
+Task: So, further introduce Kerr geodesic.
+Category: 1
+Retrieval terms: [In the form of lecture, introduce Kerr geodesic from 'constants of motion' associated with Killing vector/tensor, and 'seperable Kerr geodesic equation', as well as its analytical solutions.","Give a comprehensive description of orbital dynamics and properties of 'bound Kerr geodesic', such as 'Mino/BL frequecy'.","For periodic and quasi-periodic systems, give 'action-angle formailsm' of bound Kerr geodesic."]
 ----------------
 History: 
 """
 Q: Introduce Kerr spacetime in detail.
 A: Ok, Let's discuss the Kerr spacetime and its symetry, ......
 """
-Original question: So, further give/derive Kerr geodesic equation.
+Task: So, further give/derive Kerr geodesic equation.
+Category: 4
 Retrieval terms: ["In the form of lecture, please strictly derive 'seperable Kerr geodesic equation' and its 'analytical solutions'."]
 ----------------
 History: 
@@ -47,7 +58,8 @@ History:
 Q: Introduce Kerr geodesic in detailed.
 A: Kerr spacetime is a solution to the Einstein field equations that describes the geometry of spacetime, ......
 """
-Original question: Further derive the analytical solution of bound Kerr geodesic in terms of elliptic functions.
+Task: Further derive the analytical solution of bound Kerr geodesic in terms of elliptic functions.
+Category: 4
 Retrieval terms: []
 ----------------
 History: 
@@ -57,7 +69,8 @@ A: The current conversation is about the quantum field theory.
 Q: How do we understand Feynman diagrams?
 A: A Feynman diagram represents a perturbative contribution to the amplitude of a quantum transition from some initial quantum state to some final quantum state, ......
 """
-Original question: Can you strictly derive Compton scattering? I need more detail.
+Task: Can you strictly derive Compton scattering? I need more detail.
+Category: 4
 Retrieval terms: ["Give the mathematical derivation of 'Compton scattering'. Consider from initial and final state, then we can compute 'S-matrix element' and 'differential scattering cross section' from 'Feynman diagrams'."]
 ----------------
 History: 
@@ -67,13 +80,15 @@ A: The current conversation is about the cosmology.
 Q: I want to work out the history of the falling temperature of the early universe.
 A: The early universe underwent a sequence of pivotal events as it expanded and cooled. The cooling rate was tied to the Hubble parameter, signifying that it was proportional to the inverse of the universe's age at any given point. This cooling journey can be divided into several key epochs, ......
 """
-Original question: take a brief look at the thermodynamics and statistical mechanics of different matter, in thermal equilibrium with negligible chemical potentials. Derive the contribution of different matter to the energy density, pressure, and entropy.
-Retrieval terms: ["To describe the thermal equilibrium behavior in cosmology, we develop the model to calculate the contribution of each species of particle to the energy density, pressure, and entropy ('Fermi–Dirac or Bose–Einstein distributions')."]
+Task: take a brief look at the thermodynamics and statistical mechanics of different matter, in thermal equilibrium with negligible chemical potentials. Derive the contribution of different matter to the energy density, pressure, and entropy.
+Category: 4
+Retrieval terms: ["To describe the thermal equilibrium behavior in cosmology, we develop the model to calculate the contribution of each species of particle to the energy density, pressure, and entropy (by 'Fermi–Dirac or Bose–Einstein distributions')."]
 ----------------
 History: 
 """
 """
-Original question: How does the Galois correspondence demonstrate the relationship between field extensions and subgroups of the Galois group?
+Task: How does the Galois correspondence demonstrate the relationship between field extensions and subgroups of the Galois group?
+Category: 3
 Retrieval terms: ["First, let's disccuss the relationship between field extensions and subgroups of the 'Galois group' through 'Galois correspondence', which include the correspondence of fixed fields, intermediate fields, and 'Galois groups'.","To deepen our understanding, let's we start to discuss examples and counterexamples of Galois correspondence in specific cases, such as the splitting field of a polynomial or the field extension of a finite field."]
 ----------------
 History: 
@@ -81,7 +96,8 @@ History:
 Q: Conversation background.
 A: The current conversation is about the Instabilities and phase transitions in electronic systems.
 """
-Original question: How to describe of spin-density wave?
+Task: How to describe of spin-density wave?
+Category: 3,4
 Retrieval terms: ["Well, the instabilities and phase transitions in electronic systems is interesting. 'Spin density waves' are one of the important phenomena. Model it and obtain its energy spectrum."]
 ----------------
 History: 
@@ -89,40 +105,39 @@ History:
 Q: Conversation background.
 A: The current conversation is about Boson stars.
 """
-Original question: 列举出玻色星的重要文献和学者。
+Task: 列举出玻色星的重要文献和学者。
+Category: 4
 Retrieval terms: ["In order to better understand the research field of 'Boson stars', list all important studies and reference on Boson stars, include textbook, reviews, introduction, lecture, guide and survey. Of course, researcher is also important."]
-
 ----------------
 History: 
 """
 """
-Original question: 使用弯曲时空QFT推导Hawking辐射（使用中文回复）。
-Retrieval terms: ["In this case, we make use of quantum field theory to derive 'Hawking radiation spectrum' in the Schwarzschild black hole spacetime."]
-
-## Counterexample:
+Task: 如何理解旋量（使用中文回复）。
+Category: 3
+Retrieval terms: ["How to understand spinor in physics and mathematics? What is its definition?","What is the role of spinors in differential geometry and group representation theory?"]
 ----------------
 History: 
 """
 """
-Original question: Give examples or approximate expression of "spin weighted spheroidal function".
-Retrieval terms (Worse): ["Introduce the concept of spin-weighted spheroidal harmonics (SWSH) as solutions to the spin-weighted spheroidal wave equation (SWSE)","Provide examples of SWSH in various contexts, such as in the study of gravitational waves and black hole perturbation theory","Explain the approximate expression of SWSH in terms of spin-weighted spherical harmonics (SWSH)"]
-Retrieval terms (Better): []
+Task: Write a detailed introduction to 'Taiji Program in Space'.
+Category: 5 (Assuming you don't know 'Taiji Program in Space')
+Retrieval terms: []
 
 ## Requirements
-- Each "retrieval terms" should contain key information of "original question", yet should be an extension of "original question" with different focuses.
-- Generally, you should avoid repeating content that has already been mentioned in [History]. Such as the first case of Kerr geodesic, its "retrieval terms" don't include the definition of Kerr spacetime which has been mentioned.
-- Think about when list of "retrieval terms" should be (horizontally and generally) broader and when list of "retrieval terms" should be (vertically) deeper, specific and detailed. Here is a judgment method. The more general "original question", the more "retrieval terms" return in list; The more specific "original question", the fewer "retrieval terms" return in list and the more detailed and specific the content of "retrieval terms" is.
-- Must be honest and carefully. For specific or unknown "original question", just generate no "retrieval terms" honestly. Such as the case in [Counterexample], spin-weighted spheroidal function is SWSOH instead of SWSH. You don't know, thus return empty list.
-- "retrieval terms" must be in English. If "original question" contains Chinese characters, "retrieval terms" must include at least it's English version such as Chinese cases in examples.
+- Each "retrieval terms" should contain key information of "Task", yet should be an extension of "Task" with different focuses.
+- Generally, you should avoid repeating content that has already been mentioned in [History]. 
+- The more specific "Task", the fewer "retrieval terms" return in list and the more detailed and specific the content of "retrieval terms" is.
+- Must be honest and carefully. For specific or unknown "Task", just generate no "retrieval terms" honestly. Such as Category 5.
+- "retrieval terms" must be in English.
 
 # Initialization
-Please generate a list with 0~3 "retrieval terms", let's begin.
+Please generate a list with "retrieval terms", let's begin.
 ----------------
 History:
 """
 {{histories}}
 """
-Original question: {{query}}
+Task: {{query}}
 Retrieval fragments: `;
 
 export const queryExtension = async ({
